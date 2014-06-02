653efa0b9db7716e61dbed1ff881e37fa91b7a9f
Marco Carnevale <marco.carnevale@gmail.com>
2014-06-02 12:43:18 -0400

    Initial Version

---
+++ AndroidApp/src/com/example/garagedoorproject/Constants.java
@@ -0,0 +1,12 @@
+package com.example.garagedoorproject;
+
+/**
+ * Don't check in this file as it will have the IP of my service shown in it.
+ * 
+ * @author Marco Carnevale
+ *
+ */
+public final class Constants {
+	
+	public final static String RPI_IP = "<IP_AND_URL_To_The_GarageDoor_Service>";	
+}
---
+++ AndroidApp/src/com/example/garagedoorproject/MainActivity.java
@@ -0,0 +1,104 @@
+package com.example.garagedoorproject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		if (savedInstanceState == null) {
+			getFragmentManager().beginTransaction()
+					.add(R.id.container, new PlaceholderFragment()).commit();
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	/**
+	 * A placeholder fragment containing a simple view.
+	 */
+	public static class PlaceholderFragment extends Fragment {
+
+		public PlaceholderFragment() {
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_main, container,
+					false);
+			return rootView;
+		}
+	}
+
+	public void operateGarage(View view) {
+		// The IP address needs to be changed according to your RPi box
+		
+		
+		HttpClient httpclient = new DefaultHttpClient();
+		HttpPost httppost = new HttpPost(Constants.RPI_IP);
+		
+		try {
+			// Add your data
+			List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+			nameValuePairs.add(new BasicNameValuePair("OPEN", ""));
+			httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+			// Execute HTTP Post Request
+			HttpResponse response = httpclient.execute(httppost);			
+			Toast.makeText(this, "Success", Toast.LENGTH_LONG).show();
+		} catch (ClientProtocolException e) {
+			Log.e("GarageDoorOpener", "ClientProtocolException while calling RaspberryPi: "+ e);
+			Toast.makeText(this, "ClientProtocolException while calling RaspberryPi", Toast.LENGTH_LONG).show();
+		} catch (IOException e) {
+			Log.e("GarageDoorOpener", "IOException while calling RaspberryPi: "+ e);
+			Toast.makeText(this, "IOException while calling RaspberryPi", Toast.LENGTH_LONG).show();
+		} catch (Exception e) {
+			Log.e("GarageDoorOpener", "Exception while calling RaspberryPi: "+ e);
+			Toast.makeText(this, "Exception while calling RaspberryPi", Toast.LENGTH_LONG).show();
+		}
+	}
+
+}
